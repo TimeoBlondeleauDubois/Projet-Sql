b10f8f56f459ef9626aaddec1c674b729cb9ca7c
Timéo Blondeleau Dubois <150905525+TimeoBlondeleauDubois@users.noreply.github.com>
2023-12-05 12:05:06 +0200

    Add files via upload

--- jmerisesql.sql
+++ jmerisesql.sql
@@ -4,14 +4,14 @@
 
 
 /*------------------------------------------------------------
--- Table: serveurs
+-- Table: Détail
 ------------------------------------------------------------*/
-CREATE TABLE serveurs(
-	ID_serveurs                 INT IDENTITY (1,1) NOT NULL ,
-	Ser_Nom_Du_Serveur          VARCHAR (50) NOT NULL ,
-	Ser_Adresse_Ip_Du_Serveur   VARCHAR (50) NOT NULL ,
-	Ser_Image_Du_Serveur        NUMERIC (-1,-1)  NOT NULL  ,
-	CONSTRAINT serveurs_PK PRIMARY KEY (ID_serveurs)
+CREATE TABLE Detail(
+	ID_Detail                   INT IDENTITY (1,1) NOT NULL ,
+	det_Nom_Du_Serveur          VARCHAR (50) NOT NULL ,
+	det_Adresse_Ip_Du_Serveur   VARCHAR (50) NOT NULL ,
+	det_Image_Du_Serveur        NUMERIC (-1,-1)  NOT NULL  ,
+	CONSTRAINT Detail_PK PRIMARY KEY (ID_Detail)
 );
 
 
@@ -36,28 +36,17 @@ CREATE TABLE version(
 
 
 /*------------------------------------------------------------
--- Table: recherche
+-- Table: serveur
 ------------------------------------------------------------*/
-CREATE TABLE recherche(
-	ID_serveurs   INT  NOT NULL ,
-	ID_Tag        INT  NOT NULL  ,
-	CONSTRAINT recherche_PK PRIMARY KEY (ID_serveurs,ID_Tag)
+CREATE TABLE serveur(
+	ID_Tag       INT  NOT NULL ,
+	ID_Version   INT  NOT NULL ,
+	ID_Detail    INT  NOT NULL  ,
+	CONSTRAINT serveur_PK PRIMARY KEY (ID_Tag,ID_Version,ID_Detail)
 
-	,CONSTRAINT recherche_serveurs_FK FOREIGN KEY (ID_serveurs) REFERENCES serveurs(ID_serveurs)
-	,CONSTRAINT recherche_tag0_FK FOREIGN KEY (ID_Tag) REFERENCES tag(ID_Tag)
-);
-
-
-/*------------------------------------------------------------
--- Table: Appartient
-------------------------------------------------------------*/
-CREATE TABLE Appartient(
-	ID_Version    INT  NOT NULL ,
-	ID_serveurs   INT  NOT NULL  ,
-	CONSTRAINT Appartient_PK PRIMARY KEY (ID_Version,ID_serveurs)
-
-	,CONSTRAINT Appartient_version_FK FOREIGN KEY (ID_Version) REFERENCES version(ID_Version)
-	,CONSTRAINT Appartient_serveurs0_FK FOREIGN KEY (ID_serveurs) REFERENCES serveurs(ID_serveurs)
+	,CONSTRAINT serveur_tag_FK FOREIGN KEY (ID_Tag) REFERENCES tag(ID_Tag)
+	,CONSTRAINT serveur_version0_FK FOREIGN KEY (ID_Version) REFERENCES version(ID_Version)
+	,CONSTRAINT serveur_Detail1_FK FOREIGN KEY (ID_Detail) REFERENCES Detail(ID_Detail)
 );
 
 
