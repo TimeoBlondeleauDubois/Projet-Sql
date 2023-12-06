2e336ee516ea82d3bca9eb34b8555ca8f9a87231
Blondeleau <timeo.blondeleaudubois@viacesi.fr>
2023-12-06 15:45:50 +0100

    re

--- verification2.php
+++ verification2.php
@@ -46,8 +46,9 @@
                 echo "<div align='center'><strong>Nom du serveur :</strong> " . $detail['det_Nom_Du_Serveur'] . "<br>";
 
                 // Afficher l'image
+                
                 if (!empty($detail['det_Image_Du_Serveur'])) {
-                    echo "<img src='" . $detail['det_Image_Du_Serveur'] . "' alt='Image du serveur'><br>";
+                  echo "<img src='" . $detail['det_Image_Du_Serveur'] . "' alt='Image du serveur' style='max-width: 100%; max-height: 300px; height: auto; margin-top: 10px; border-radius: 8px;'><br>";
                 }
 
                 echo "<strong>Adresse IP du serveur :</strong> " . $detail['det_Adresse_Ip_Du_Serveur'] . "<br>";
